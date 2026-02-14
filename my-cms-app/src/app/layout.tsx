24b7bdbcbcf31044e687b3185bf38c1fed8a6016
Adorarin GolfinS <aiiadorarin@gmail.com>
2026-02-14 15:30:16 +0700

    Clean CMS-backend

--- my-cms-app/src/app/layout.tsx
+++ my-cms-app/src/app/layout.tsx
@@ -2,7 +2,6 @@
 
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-// import { ClerkProvider } from '@clerk/nextjs'
 import "./globals.css";
 
 const geistSans = Geist({
--- my-cms-app/src/lib/getParentId.ts
+++
@@ -1,4 +0,0 @@
-export function getTempParentId(): string {
-  // TODO: Replace with actual parent_id from your database
-  return 'REPLACE_WITH_REAL_PARENT_ID';
-}
